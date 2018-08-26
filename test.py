911ef46e6152d6908e7e79359709691f4dcffcd8
jialpeng <jialpeng@cisco.com>
2018-08-26 10:28:03 +0800

    fix 2

--- test.py
+++ test.py
@@ -8,47 +8,43 @@ import subprocess
 
 
 testcase_dict=collections.OrderedDict()
+test_path=''
 
-def common_setup(cmdopt):
-    global testcase_dict
-    if cmdopt == "onboard":
-        test_path = '/root/tengine/jenkins'
-    elif cmdopt == "cc":
-        test_path = '/root/cross_test'
-    print(test_path)
-    testlist=asciitable.read(test_path+'/core_test.list')
-    
-    for rec in testlist:
-        testcase_dict[rec[0]]=rec[1]
-        #pdb.set_trace()
-    pprint.pprint(dict(testcase_dict.items()))
-
-
-
-
-#pdb.set_trace()
+class TestClass(object):
+    def common_setup(self,cmdopt):
+        global testcase_dict
+        global test_path
+        if cmdopt == "onboard":
+            test_path = '/root/tengine/jenkins'
+        elif cmdopt == "cc":
+            test_path = '/root/cross_test'
+        print(test_path)
+        testlist=asciitable.read(test_path+'/core_test.list')
+        
+        for rec in testlist:
+            testcase_dict[rec[0]]=rec[1]
+            #pdb.set_trace()
+        pprint.pprint(dict(testcase_dict.items()))
 
 
-
-
-
-@pytest.mark.parametrize("id",testcase_dict.keys())
-#@pytest.mark.parametrize("id",testcase_dict.keys()[1:2])
-def test_eval(id,cmdopt):
-    #print "testcase_id=%s,path=%s" % (id,testcase_dict[id])
-    if cmdopt == "onboard":
-        try:
-            subprocess.check_output(test_path+"/test.sh %s"%id,shell=True)
-            result=True
-        except subprocess.CalledProcessError,ex:
-            print ex.output # contains stdout and stderr together 
-            result=False
-        assert result==True
-    elif cmdopt == "cc":
-        try:
-            subprocess.check_output(test_path+"/test.sh %s"%id,shell=True)
-            result=True
-        except subprocess.CalledProcessError,ex:
-            print ex.output # contains stdout and stderr together 
-            result=False
-        assert result==True
+    @pytest.mark.parametrize("id",testcase_dict.keys())
+    #@pytest.mark.parametrize("id",testcase_dict.keys()[1:2])
+    def test_eval(self,id,cmdopt):
+        #print "testcase_id=%s,path=%s" % (id,testcase_dict[id])
+        global test_path
+        if cmdopt == "onboard":
+            try:
+                subprocess.check_output(test_path+"/test.sh %s"%id,shell=True)
+                result=True
+            except subprocess.CalledProcessError,ex:
+                print ex.output # contains stdout and stderr together 
+                result=False
+            assert result==True
+        elif cmdopt == "cc":
+            try:
+                subprocess.check_output(test_path+"/test.sh %s"%id,shell=True)
+                result=True
+            except subprocess.CalledProcessError,ex:
+                print ex.output # contains stdout and stderr together 
+                result=False
+            assert result==True
