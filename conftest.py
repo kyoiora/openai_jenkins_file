f404689b09128ca1ddcd5be946830903044e6ae2
jialpeng <jialpeng@cisco.com>
2018-08-26 12:08:55 +0800

    test

--- conftest.py
+++ conftest.py
@@ -7,18 +7,19 @@ def pytest_addoption(parser):
     )
 
 
+
 def pytest_generate_tests(metafunc):
-    if 'testcase_dict' in metafunc.fixturenames:
+	if 'testcase_dict' in metafunc.fixturenames:
 		if metafunc.config.getoption('cmdopt') == "onboard":
-		    test_path = '/root/tengine/jenkins'
+			test_path = '/root/tengine/jenkins'
 		elif metafunc.config.getoption('cmdopt') == "cc":
-		    test_path = '/root/cross_test'
+			test_path = '/root/cross_test'
 		testcase_list=collections.OrderedDict()
 		print(test_path)
 		testlist=asciitable.read(test_path+'/core_test.list')
 
 		for rec in testlist:
-		    testcase_list[rec[0]]=rec[1]
-		    #pdb.set_trace()
+			testcase_list[rec[0]]=rec[1]
+			#pdb.set_trace()
 		pprint.pprint(dict(testcase_dict.items()))
-	    metafunc.parametrize("testcase_dict", testcase_list.key())
+		metafunc.parametrize("testcase_dict", testcase_list.key())
