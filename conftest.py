33b07366ad6775aadefbe93a37d89435b5794c6b
jialpeng <jialpeng@cisco.com>
2018-08-26 12:06:22 +0800

    no message

--- conftest.py
+++ conftest.py
@@ -7,6 +7,18 @@ def pytest_addoption(parser):
     )
 
 
-@pytest.fixture
-def cmdopt(request):
-    return request.config.getoption("--cmdopt")
+def pytest_generate_tests(metafunc):
+    if 'testcase_dict' in metafunc.fixturenames:
+		if metafunc.config.getoption('cmdopt') == "onboard":
+		    test_path = '/root/tengine/jenkins'
+		elif metafunc.config.getoption('cmdopt') == "cc":
+		    test_path = '/root/cross_test'
+		testcase_list=collections.OrderedDict()
+		print(test_path)
+		testlist=asciitable.read(test_path+'/core_test.list')
+
+		for rec in testlist:
+		    testcase_list[rec[0]]=rec[1]
+		    #pdb.set_trace()
+		pprint.pprint(dict(testcase_dict.items()))
+	    metafunc.parametrize("testcase_dict", testcase_list.key())
--- test.py
+++ test.py
@@ -9,22 +9,11 @@ import subprocess
 pprint.pprint(variables)
 print "cmdopt=" + cmdopt
 
-if cmdopt == "onboard":
-    test_path = '/root/tengine/jenkins'
-elif cmdopt == "cc":
-    test_path = '/root/cross_test'
-testcase_dict=collections.OrderedDict()
-print(test_path)
-testlist=asciitable.read(test_path+'/core_test.list')
-
-for rec in testlist:
-    testcase_dict[rec[0]]=rec[1]
-    #pdb.set_trace()
-pprint.pprint(dict(testcase_dict.items()))
 
 
 
-@pytest.mark.parametrize("id",testcase_dict.keys())
+
+@pytest.mark.parametrize("id",testcase_dict)
 #@pytest.mark.parametrize("id",testcase_dict.keys()[1:2])
 def test_eval(self,id):
     #print "testcase_id=%s,path=%s" % (id,testcase_dict[id])
