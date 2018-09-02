e16ac0adb2977cc0b629b28228daf867c4853464
jialpeng <jialpeng@cisco.com>
2018-09-02 23:54:51 +0800

    android

--- push_android.py
+++ push_android.py
@@ -1,41 +1,34 @@
+import os,pdb
 
-import argparse
-import pdb
-import sys
+sys.path.append(test_path + '/libs')
+
 from adb_wrapper.adb_wrapper.adb_wrapper import AdbWrapper
+import argparse
 ROOT_DIR='/root/tengine_armv8/android_pack/'
 TARGET_DIR_ON_BOARD='/data/local/tmp/'
-
-
 def push():
-
     a = AdbWrapper() # Auto Find adb in system PATH or Environment
-    a.connect(android_ip)
+    a.connect("192.168.50.114")
     #pdb.set_trace()
-    a.device=android_ip
+    a.device='192.168.50.114'
     a.push(ROOT_DIR, TARGET_DIR_ON_BOARD)
     res=a.shell("ls -al %s"%(TARGET_DIR_ON_BOARD+'android_pack/'))
+    res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/*.so'))
+        res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/Classify'))
     print(res[0])
     res=a.shell("cd %s && chmod u+x Classify "%(TARGET_DIR_ON_BOARD+'android_pack/'))
 
     res=a.shell("ls -al %s"%(TARGET_DIR_ON_BOARD+'android_pack/'))
-
+    
     #pdb.set_trace()
     print(res[0])
 
 
 
-def Classify1(android_ip):
-    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
-    a = AdbWrapper() # Auto Find adb in system PATH or Environment
-    a.connect(android_ip)
-    #pdb.set_trace()
-    a.device=android_ip
 
-    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -i %s/cat.jpg "%(target_dir,target_dir,target_dir,target_dir))
-    #pdb.set_trace()
-    print(res[0])
-    print(res[1])
-
-def Classify2(android_ip):
-    pass
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--func", help="call function", required=False)
+    args = parser.parse_args()
+    if args.func=="push":
+        push()
