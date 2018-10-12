3897e47f60cd1169c9a18ac0cbe1adf519d1c036
jiaang2001 <wxjiang@openailab.com>
2018-10-12 11:29:14 +0800

    Run convate

--- test.py
+++ test.py
@@ -46,10 +46,3 @@ def test_eval_android(testcase_dict,cmdopt,targetdir,ip):
     #     print ex.output # contains stdout and stderr together 
     #     result=False
     # assert result==True
-
-def test_eval_convert(testcase_dict,cmdopt,targetdir,variables):
-    #print "testcase_id=%s,path=%s" % (id,testcase_dict[id])
-
-    tc="converttest.%s(%s)"%(testcase_dict)
-    print tc
-    test=eval(tc)
