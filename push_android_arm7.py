7c845693a4e15f776d9f68a3d0cd6b855fba7c15
jiaang2001 <wxjiang@openailab.com>
2018-10-19 15:41:31 +0800

    android_arm32

--- push_android_arm7.py
+++ push_android_arm7.py
@@ -17,6 +17,7 @@ def push(ip_addr):
     res=a.shell("ls -al %s"%(TARGET_DIR_ON_BOARD+'android_pack/'))
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/*.so'))
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/Classify'))
+    print res
     print 'push to board'
     a.push(ROOT_DIR, TARGET_DIR_ON_BOARD)
     print 'push ok'
