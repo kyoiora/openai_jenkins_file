6e4befab85574f76fe0bc6a940900e200f39f7e8
jiaang2001 <wxjiang@openailab.com>
2018-10-23 12:47:59 +0800

    android_arm32

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ def test_eval(testcase_dict,cmdopt,targetdir,variables):
 
     #eval testcases from linux_testcases.linuxtestcases.py
     print "testcase_dict=%s"%testcase_dict
+    print os.system(pwd)
     ltc=os.system("python C:\Users\user\WebstormProjects\openai_jenkins_file\linux_testcases\linuxtestcases.py")
     print ltc
 
