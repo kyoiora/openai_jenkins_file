372c18a9dcb17a743078d20e6e92f99458bcd7b0
jiaang2001 <wxjiang@openailab.com>
2018-10-23 16:12:06 +0800

    android_arm32

--- linux_testcases/linuxtestcases.py
+++ linux_testcases/linuxtestcases.py
@@ -5,7 +5,7 @@ import sys
 TARGET_DIR_ON_BOARD='/home/rk/tengine/examples/build/imagenet_classification'
 
 def imagenet_sqz():
-    target_dir=TARGET_DIR_ON_BOARD
+    target_dir=target_dir+'/examples/build/imagenet_classification'
     res=os.popen("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n squeezenet "%(target_dir,target_dir,target_dir),"r")
     out=res.read()
     print(out)
