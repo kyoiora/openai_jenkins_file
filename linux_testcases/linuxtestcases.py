72318322eb5e652b8605d92da2e04062fb3596ef
jiaang2001 <wxjiang@openailab.com>
2018-10-23 12:54:09 +0800

    android_arm32

--- linux_testcases/linuxtestcases.py
+++ linux_testcases/linuxtestcases.py
@@ -9,4 +9,5 @@ def imagenet_sqz():
     res=shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n squeezenet "%(target_dir,target_dir,target_dir))
     print(res[0])
     assert "0.2763 - \"n02123045" in res[0]
-    imagenet_sqz()
+
+imagenet_sqz()
