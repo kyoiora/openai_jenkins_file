aa092c4f7add70f25d84f6e09cd8543011eda8d1
jiaang2001 <wxjiang@openailab.com>
2018-10-23 11:22:40 +0800

    android_arm32

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def test_eval(testcase_dict,cmdopt,targetdir,variables):
 
     #eval testcases from linux_testcases.linuxtestcases.py
     print "testcase_dict=%s"%testcase_dict
-    tc="linuxtestcases.%s(%s)"%(testcase_dict)
+    tc="linuxtestcases.%s(%s)"%(testcase_dict,board_ip)
     print tc
     test=eval(tc)
 
