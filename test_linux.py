f62f678289ee4b5e07c8ab6664415ad9a23b9a19
jiaang2001 <wxjiang@openailab.com>
2018-10-23 14:39:49 +0800

    android_arm32

--- test_linux.py
+++ test_linux.py
@@ -13,10 +13,11 @@ TARGET_DIR_ON_BOARD='/home/rk/tengine/examples/build/imagenet_classification'
 
 linux_testlist=['sqz']
 
-def sqz(self):
-    target_dir=TARGET_DIR_ON_BOARD
-    res=os.popen("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n squeezenet "%(target_dir,target_dir,target_dir))
-    out=res.readlines()
-    print(out)
-    assert "0.2763 - \"n02123045" in out
-print "OK"
+class test():
+    def sqz(self):
+        target_dir=TARGET_DIR_ON_BOARD
+        res=os.popen("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n squeezenet "%(target_dir,target_dir,target_dir))
+        out=res.readlines()
+        print(out)
+        assert "0.2763 - \"n02123045" in out
+test()
