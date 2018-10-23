245f2d2e3ebfc6c9be0eb3f20d2b9d50f360b61e
jiaang2001 <wxjiang@openailab.com>
2018-10-23 15:57:38 +0800

    android_arm32

--- linux_testcases/linuxtestcases.py
+++ linux_testcases/linuxtestcases.py
@@ -11,3 +11,83 @@ def imagenet_sqz():
     print(out)
     assert "0.2763 - \"n02123045" in out
 
+def imagenet_mobilenet(a):
+    target_dir=TARGET_DIR_ON_BOARD
+    res=os.popen("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n mobilenet "%(target_dir,target_dir,target_dir),"r")
+    out=res.read()
+    print(out)
+    assert "8.5976 - \"n02123159" in out
+
+def imagenet_alexnet(android_ip):
+    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
+    a = AdbWrapper() # Auto Find adb in system PATH or Environment
+    a.connect(android_ip)
+    #pdb.set_trace()
+    a.device=android_ip
+
+    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n alexnet "%(target_dir,target_dir,target_dir))
+    #pdb.set_trace()
+    print(res[0])
+    assert "0.3094 - \"n02124075" in res[0]
+
+def imagenet_googlenet(android_ip):
+    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
+    a = AdbWrapper() # Auto Find adb in system PATH or Environment
+    a.connect(android_ip)
+    #pdb.set_trace()
+    a.device=android_ip
+
+    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n googlenet "%(target_dir,target_dir,target_dir))
+    #pdb.set_trace()
+    print(res[0])
+    assert "0.5009 - \"n02123159" in res[0]
+
+
+
+def imagenet_inception_v3(android_ip):
+    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
+    a = AdbWrapper() # Auto Find adb in system PATH or Environment
+    a.connect(android_ip)
+    #pdb.set_trace()
+    a.device=android_ip
+
+    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n inception_v3 "%(target_dir,target_dir,target_dir))
+    #pdb.set_trace()
+    print(res[0])
+    assert "0.0946 - \"n02123159" in res[0]
+
+def imagenet_inception_v4(android_ip):
+    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
+    a = AdbWrapper() # Auto Find adb in system PATH or Environment
+    a.connect(android_ip)
+    #pdb.set_trace()
+    a.device=android_ip
+
+    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n inception_v4 "%(target_dir,target_dir,target_dir))
+    #pdb.set_trace()
+    print(res[0])
+    assert "0.7556 - \"n02123159" in res[0]
+
+def imagenet_resnet50(android_ip):
+    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
+    a = AdbWrapper() # Auto Find adb in system PATH or Environment
+    a.connect(android_ip)
+    #pdb.set_trace()
+    a.device=android_ip
+
+    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n resnet50 -i %s/tests/images/bike.jpg"%(target_dir,target_dir,target_dir,target_dir))
+    #pdb.set_trace()
+    print(res[0])
+    assert "0.9239 - \"n03792782" in res[0]
+
+def imagenet_vgg16(android_ip):
+    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
+    a = AdbWrapper() # Auto Find adb in system PATH or Environment
+    a.connect(android_ip)
+    #pdb.set_trace()
+    a.device=android_ip
+
+    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n vgg16 -i %s/tests/images/bike.jpg"%(target_dir,target_dir,target_dir,target_dir))
+    #pdb.set_trace()
+    print(res[0])
+    assert "0.4998 - \"n03792782" in res[0]
