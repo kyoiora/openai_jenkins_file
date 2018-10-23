1f5c4a1e8f57827a716035fb095c2457a6a90b24
jiaang2001 <wxjiang@openailab.com>
2018-10-23 16:07:04 +0800

    android_arm32

--- linux_testcases/linuxtestcases.py
+++ linux_testcases/linuxtestcases.py
@@ -53,14 +53,9 @@ def imagenet_alexnet():
     print(out)
     assert "0.3094 - \"n02124075" in out
 
-def imagenet_vgg16(android_ip):
-    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
-    a = AdbWrapper() # Auto Find adb in system PATH or Environment
-    a.connect(android_ip)
-    #pdb.set_trace()
-    a.device=android_ip
-
-    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n vgg16 -i %s/tests/images/bike.jpg"%(target_dir,target_dir,target_dir,target_dir))
-    #pdb.set_trace()
-    print(res[0])
-    assert "0.4998 - \"n03792782" in res[0]
+def imagenet_vgg16():
+    target_dir=TARGET_DIR_ON_BOARD
+    res=os.popen("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n vgg16 -i %s/tests/images/bike.jpg"%(target_dir,target_dir,target_dir,target_dir),"r")
+    out=res.read()
+    print(out)
+    assert "0.4998 - \"n03792782" in out
