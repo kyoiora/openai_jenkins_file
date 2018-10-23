9b0485d71d967535f226fb4b3c51724cdf36a66b
jiaang2001 <wxjiang@openailab.com>
2018-10-23 16:06:15 +0800

    android_arm32

--- linux_testcases/linuxtestcases.py
+++ linux_testcases/linuxtestcases.py
@@ -32,43 +32,26 @@ def imagenet_googlenet():
     print(out)
     assert "0.5009 - \"n02123159" in out
 
-def imagenet_inception_v4(android_ip):
-    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
-    a = AdbWrapper() # Auto Find adb in system PATH or Environment
-    a.connect(android_ip)
-    #pdb.set_trace()
-    a.device=android_ip
+def imagenet_inception_v4():
+    target_dir=TARGET_DIR_ON_BOARD
+    res=os.popen("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n inception_v4 "%(target_dir,target_dir,target_dir),"r")
+    out=res.read()
+    print(out)
+    assert "0.7556 - \"n02123159" in out
 
-    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n inception_v4 "%(target_dir,target_dir,target_dir))
-    #pdb.set_trace()
-    print(res[0])
-    assert "0.7556 - \"n02123159" in res[0]
+def imagenet_inception_v3():
+    target_dir=TARGET_DIR_ON_BOARD
+    res=os.popen("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n inception_v3 "%(target_dir,target_dir,target_dir),"r")
+    out=res.read()
+    print(out)
+    assert "0.0946 - \"n02123159" in out
 
-def imagenet_inception_v3(android_ip):
-    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
-    a = AdbWrapper() # Auto Find adb in system PATH or Environment
-    a.connect(android_ip)
-    #pdb.set_trace()
-    a.device=android_ip
-
-    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n inception_v3 "%(target_dir,target_dir,target_dir))
-    #pdb.set_trace()
-    print(res[0])
-    assert "0.0946 - \"n02123159" in res[0]
-
-
-
-def imagenet_alexnet(android_ip):
-    target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
-    a = AdbWrapper() # Auto Find adb in system PATH or Environment
-    a.connect(android_ip)
-    #pdb.set_trace()
-    a.device=android_ip
-
-    res=a.shell("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n alexnet "%(target_dir,target_dir,target_dir))
-    #pdb.set_trace()
-    print(res[0])
-    assert "0.3094 - \"n02124075" in res[0]
+def imagenet_alexnet():
+    target_dir=TARGET_DIR_ON_BOARD
+    res=os.popen("cd %s;export LD_LIBRARY_PATH=%s; %s/Classify -n alexnet "%(target_dir,target_dir,target_dir),"r")
+    out=res.read()
+    print(out)
+    assert "0.3094 - \"n02124075" in out
 
 def imagenet_vgg16(android_ip):
     target_dir=TARGET_DIR_ON_BOARD+'android_pack/'
