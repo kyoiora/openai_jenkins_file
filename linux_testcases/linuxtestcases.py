e982eaa476a5de0caa2d0bb946d723b8e8168a33
jiaang2001 <wxjiang@openailab.com>
2018-10-25 14:55:38 +0800

    android_arm32

--- linux_testcases/linuxtestcases.py
+++ linux_testcases/linuxtestcases.py
@@ -68,6 +68,7 @@ def ssd():
     out=res.read()
     print(out)
     arr=out.splitlines()
+    #check the car
     a=re.findall(r"\d+\.?\d*", arr[9])
     x0=float(a[0])
     y0=float(a[1])
@@ -81,3 +82,17 @@ def ssd():
     assert(x1>690),"car x1 less than 691"
     assert(y1<177),"car y1 more than 176"
     assert(y1>165),"car y1 less than 166"
+    #check the bicycle
+    a=re.findall(r"\d+\.?\d*", arr[11])
+    x0=float(a[0])
+    y0=float(a[1])
+    x1=float(a[2])
+    y1=float(a[3])
+    assert(x0<149),"car x0 more than 148"
+    assert(x0>137),"car x0 less than 138"
+    assert(y0<144),"car y0 more than 143"
+    assert(y0>132),"car y0 less than 133"
+    assert(x1<585),"car x1 more than 584"
+    assert(x1>573),"car x1 less than 574"
+    assert(y1<510),"car y1 more than 509"
+    assert(y1>498),"car y1 less than 499"
