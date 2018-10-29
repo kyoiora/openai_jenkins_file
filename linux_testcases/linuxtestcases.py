9b99f0bec5134341beb7d066481221b865b7ad2b
jiaang2001 <wxjiang@openailab.com>
2018-10-29 13:42:17 +0800

    android_arm32

--- linux_testcases/linuxtestcases.py
+++ linux_testcases/linuxtestcases.py
@@ -1,6 +1,6 @@
 import os
 import re
-import targetdir
+import test
 ROOT_DIR=targetdir
 TARGET_DIR_ON_BOARD='examples/build/imagenet_classification'
 caffe_wrapper_sqz_dir='build/examples/caffe_wrapper/cpp_classification'
