6bbf22a866ab9f95bc1e9e37d7b498a0de0d7d96
Weixiang <wxjiang@openailab.com>
2018-10-29 15:49:09 +0800

    android_arm32

--- conftest.py
+++ conftest.py
@@ -10,6 +10,11 @@ android_testlist=['imagenet_sqz','imagenet_alexnet','imagenet_googlenet',
                   'imagenet_inception_v4',
                   'imagenet_resnet50','imagenet_vgg16']
 
+linux_testlist=['quick_api','imagenet_sqz','imagenet_alexnet','imagenet_googlenet',
+                  'imagenet_mobilenet','imagenet_inception_v3',
+                  'imagenet_inception_v4',
+                  'imagenet_resnet50','imagenet_vgg16']
+
 def pytest_addoption(parser):
 	parser.addoption(
 		"--cmdopt", action="store", default="onboard", 
@@ -39,17 +44,18 @@ def pytest_generate_tests(metafunc):
 	print metafunc.config.getoption('cmdopt')
 	# if linux test job get parmetrize from core_test.list
 	if metafunc.config.getoption('cmdopt') != "adb":
-		if 'testcase_dict' in metafunc.fixturenames:
-			test_path = metafunc.config.getoption('targetdir')
-			testcase_list=collections.OrderedDict()
-			print(test_path)
-			testlist=asciitable.read(test_path+'/jenkins/core_test.list')
+		#if 'testcase_dict' in metafunc.fixturenames:
+		#	test_path = metafunc.config.getoption('targetdir')
+		#	testcase_list=collections.OrderedDict()
+		#	print(test_path)
+		#	testlist=asciitable.read(test_path+'/jenkins/core_test.list')
 
-			for rec in testlist:
-				testcase_list[rec[0]]=rec[1]
-			pprint.pprint(dict(testcase_list.items()))
-			metafunc.parametrize("testcase_dict", testcase_list.keys())
-			
+		#	for rec in testlist:
+		#		testcase_list[rec[0]]=rec[1]
+		#	pprint.pprint(dict(testcase_list.items()))
+		#	metafunc.parametrize("testcase_dict", testcase_list.keys())
+		metafunc.parametrize("testcase_dict", linux_testlist)
+		print(linux_testlist)
 
 	else:
 		# If test job is for Android ,use the list of android_testlist to call 
