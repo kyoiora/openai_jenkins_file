09636078eeb25ec01c0b112d3768092a001f6f06
Weixiang <wxjiang@openailab.com>
2018-10-31 16:31:02 +0800

    android_arm32

--- push_android_arm8.py
+++ push_android_arm8.py
@@ -4,7 +4,7 @@ test_path = (os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(test_path + '/libs')
 from adb_wrapper.adb_wrapper.adb_wrapper import AdbWrapper
 import argparse
-ROOT_DIR='/root/tengine_armv8/android_pack/'
+ROOT_DIR='/root/tengine_arm8/tengine/android_pack/'
 TARGET_DIR_ON_BOARD='/data/local/tmp/'
 def push(ip_addr):
     a = AdbWrapper() # Auto Find adb in system PATH or Environment
@@ -13,15 +13,13 @@ def push(ip_addr):
     a.device=ip_addr
     a.root()
 
-
     res=a.shell("ls -al %s"%(TARGET_DIR_ON_BOARD+'android_pack/'))
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/*.so'))
-
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/Classify'))
-
-
-    a.push(ROOT_DIR, TARGET_DIR_ON_BOARD)
-
+    print res
+    print 'push to board'
+    a.push(ROOT_DIR,TARGET_DIR_ON_BOARD,timeout=3600)
+    print 'push ok'
     print(res[0])
     res=a.shell("\'cd %s && chmod u+x Classify\' "%(TARGET_DIR_ON_BOARD+'android_pack/'))
 
