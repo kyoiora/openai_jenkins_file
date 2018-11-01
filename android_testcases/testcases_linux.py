07c20a02d52bc5169d5a239312c6f33963a1c699
Weixiang <wxjiang@openailab.com>
2018-11-01 15:14:45 +0800

    android_arm32

--- android_testcases/testcases_linux.py
+++ android_testcases/testcases_linux.py
@@ -110,6 +110,6 @@ def imagenet_sqz_FP32_1xA17(android_ip):
     a.connect(android_ip)
     #pdb.set_trace()
     a.device=android_ip
-    res=a.shell("cd %s;export TENGINE_CPU_LIST=2;export CONV_INT_PRIO=2000;export LD_LIBRARY_PATH=%s; %s/Classify -n squeezenet -r 100;sleep 20 "%(target_dir,target_dir,target_dir))
+    res=a.shell("cd %s;export CONV_INT_PRIO=200;export TENGINE_CPU_LIST=5;%s/Classify -n squeezenet -i %s/tests/images/cat.jpg"%(target_dir,target_dir,target_dir))
     #pdb.set_trace()
     print(res[0])
