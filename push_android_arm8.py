1e9ef24c147571488fbeff1f30aeaac810a7164e
Weixiang <wxjiang@openailab.com>
2018-11-02 15:09:16 +0800

    android_arm32

--- push_android_arm8.py
+++ push_android_arm8.py
@@ -6,12 +6,13 @@ from adb_wrapper.adb_wrapper.adb_wrapper import AdbWrapper
 import argparse
 TARGET_DIR_ON_BOARD='/data/local/tmp/'
 
-if metafunc.config.getoption('cmdopt') == "linuxPT3399":
-    ROOT_DIR='/root/tengine_armv8/tengine/android_pack/'
-elif metafunc.config.getoption('cmdopt') == "linuxPT3288":
-    ROOT_DIR='/root/tengine_armv7/tinkerboard/tengine/android_pack/'
-else:
-    ROOT_DIR='/root/tengine_armv7/bananapi/tengine/android_pack/'
+def pytest_generate_tests(metafunc):
+    if metafunc.config.getoption('cmdopt') == "linuxPT3399":
+        ROOT_DIR='/root/tengine_armv8/tengine/android_pack/'
+    elif metafunc.config.getoption('cmdopt') == "linuxPT3288":
+        ROOT_DIR='/root/tengine_armv7/tinkerboard/tengine/android_pack/'
+    else:
+        ROOT_DIR='/root/tengine_armv7/bananapi/tengine/android_pack/'
 
 def push(ip_addr):
     a = AdbWrapper() # Auto Find adb in system PATH or Environment
@@ -19,7 +20,7 @@ def push(ip_addr):
     #pdb.set_trace()
     a.device=ip_addr
     a.root()
-
+    ROOT_DIR=pytest_generate_tests(metafunc)
     res=a.shell("ls -al %s"%(TARGET_DIR_ON_BOARD+'android_pack/'))
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/*.so'))
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/Classify'))
