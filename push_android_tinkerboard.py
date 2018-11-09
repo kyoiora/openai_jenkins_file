d3c72bd16d155e8c87840534b7232bf8c11cd0c8
Weixiang <wxjiang@openailab.com>
2018-11-09 16:46:20 +0800

    TEST

--- push_android_tinkerboard.py
+++ push_android_tinkerboard.py
@@ -17,7 +17,7 @@ def push(ip_addr):
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/*'))
     print res
     print 'push to board'
-    a.push(ROOT_DIR,TARGET_DIR_ON_BOARD,timeout=3600)
+    a.push(ROOT_DIR,TARGET_DIR_ON_BOARD,timeout=4800)
     print 'push ok'
     print(res[0])
     res=a.shell("\'cd %s && chmod u+x Classify\' "%(TARGET_DIR_ON_BOARD+'android_pack/'))
