833f465358513b4e8bedfb16fa8429f2d869a1b7
Weixiang <wxjiang@openailab.com>
2018-11-19 18:34:10 +0800

    TEST

--- conftest.py
+++ conftest.py
@@ -12,13 +12,13 @@ android_testlist=['imagenet_sqz','imagenet_alexnet','imagenet_googlenet',
 RK3399_PT_testlist=['squeezenet_FP32_1xA72','squeezenet_Int8_1xA72','squeezenet_FP32_2xA72','squeezenet_Int8_2xA72',
                     'squeezenet_FP32_1xA53','squeezenet_Int8_1xA53','squeezenet_FP32_4xA53','squeezenet_Int8_4xA53',
                     'mobilenet_FP32_1xA72','mobilenet_Int8_1xA72','mobilenet_FP32_2xA72','mobilenet_Int8_2xA72',
-                    'mobilenet_FP32_1xA53','mobilenet_Int8_1xA53','mobilenet_FP32_4xA53','mobilenet_Int8_4xA53','test_result',]
+                    'mobilenet_FP32_1xA53','mobilenet_Int8_1xA53','mobilenet_FP32_4xA53','mobilenet_Int8_4xA53','RK3399_testresult',]
 
 RK3288_PT_testlist=['squeezenet_FP32_1xA17','squeezenet_Int8_1xA17','squeezenet_FP32_4xA17','squeezenet_Int8_4xA17',
-                    'mobilenet_FP32_1xA17','mobilenet_Int8_1xA17','mobilenet_FP32_4xA17','mobilenet_Int8_4xA17',]
+                    'mobilenet_FP32_1xA17','mobilenet_Int8_1xA17','mobilenet_FP32_4xA17','mobilenet_Int8_4xA17','RK3288_testresult',]
 
 BananaPi_PT_testlist=['squeezenet_FP32_1xA7','squeezenet_Int8_1xA7','squeezenet_FP32_4xA7','squeezenet_Int8_4xA7',
-                    'mobilenet_FP32_1xA7','mobilenet_Int8_1xA7','mobilenet_FP32_4xA7','mobilenet_Int8_4xA7',]
+                    'mobilenet_FP32_1xA7','mobilenet_Int8_1xA7','mobilenet_FP32_4xA7','mobilenet_Int8_4xA7','Bananapi_testresult',]
 
 linux_testlist=[  'quick_api','test_two_sqz','get_node',
                   'imagenet_sqz','imagenet_alexnet','imagenet_googlenet',
