c0db717f987281af150a3da157c24acbd56570dc
Weixiang <wxjiang@openailab.com>
2018-11-19 15:59:02 +0800

    TEST

--- linux_testcases/linuxtestcases11.py
+++ linux_testcases/linuxtestcases11.py
@@ -6,6 +6,14 @@ import prettytable as pt
 
 # Performance test for RT3399
 
+def time_arr(arr):
+    for char in arr:
+        if "Repeat" in char:
+            char_arr=char.splitlines()
+            Number=arr.index(char_arr[0])
+    a=re.findall(r"\d+\.?\d*", arr[Number])
+    return a
+
 def squeezenet_FP32_1xA72(targetdir,cmdopt):
     target_dir=targetdir+TARGET_DIR_ON_BOARD
     res=os.popen("cd %s;export KERNEL_MODE=0;export TENGINE_CPU_LIST=5;export LD_LIBRARY_PATH=%s; %s/Classify -n squeezenet -r 100 & mpstat -P ALL 1 3;sleep 20"%(target_dir,target_dir,target_dir),"r")
@@ -25,11 +33,7 @@ def squeezenet_FP32_1xA72(targetdir,cmdopt):
     print("Int8_1xA72 result:\n")
     print(out)
     arr=out.splitlines()
-    for char in arr:
-        if "Repeat" in char:
-            char_arr=char.splitlines()
-            Number=arr.index(char_arr[0])
-    a=re.findall(r"\d+\.?\d*", arr[Number])
+    time_arr(arr)
     Int8_1xA72_time=a[1]
     RK3399 = pt.PrettyTable()
     RK3399.field_names = ["FP32_1xA72", "Int8_1xA72", "FP32_2xA72", "Int8_2xA72","FP32_1xA53", "Int8_1xA53", "FP32_4xA53", "Int8_4xA53"]
