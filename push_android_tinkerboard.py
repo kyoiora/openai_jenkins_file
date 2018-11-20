049ca3ac0aa175c23ee53512019bdd5ca6d82039
Weixiang <wxjiang@openailab.com>
2018-11-20 18:32:34 +0800

    TEST

--- push_android_tinkerboard.py
+++ push_android_tinkerboard.py
@@ -16,7 +16,7 @@ def push(ip_addr):
 
     res=a.shell("ls -al %s"%(TARGET_DIR_ON_BOARD+'android_pack/'))
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/*.so'))
-
+    res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/build'))
     res=a.shell("rm -rf %s"%(TARGET_DIR_ON_BOARD+'android_pack/Classify'))
 
 
